5fcd22dfdfb4626b7e64164b801b10a7a53e92d9
coopersigrist <csigrist@umass.edu>
2024-03-15 14:03:59 -0400

    added data scraping of project sunroof

---
+++ Proj_sunroof_scraping/scrape.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tabulate import tabulate
+
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+from util import state_conversion
+
+df_state = pd.read_csv('https://storage.googleapis.com/project-sunroof/csv/latest/project-sunroof-state.csv')
+df_city = pd.read_csv('https://storage.googleapis.com/project-sunroof/csv/latest/project-sunroof-city.csv')
+
+# df_electricity = pd.read_excel('https://www.eia.gov/electricity/sales_revenue_price/xls/table5_a.xlsx', engine='xlrd')
+
+
+
+df_state.info()
